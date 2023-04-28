608a55fec025584984b879741df013f7b4008ffe
LegoFigure11 <24732684+LegoFigure11@users.noreply.github.com>
2023-04-29 00:05:22 +1000

    Update offsets for SV 1.3.0

--- Structures/Offsets.cs
+++ Structures/Offsets.cs
@@ -5,10 +5,11 @@
         public const string ScarletID = "0100A3D008C5C000";
         public const string VioletID = "01008F6008C5E000";
 
-        public static string Guest1Pointer { get; } = "[[[main+44A3528]+48]+E0]";
-        public static string Guest2Pointer { get; } = "[[[main+44A3528]+48]+110]";
-        public static string Guest3Pointer { get; } = "[[[main+44A3528]+48]+140]";
+        // RaidBot.cs "Clearing stored OTs"
+        public static string Guest1Pointer { get; } = "[[[main+44B97D8]+48]+E0]";
+        public static string Guest2Pointer { get; } = "[[[main+44B97D8]+48]+110]";
+        public static string Guest3Pointer { get; } = "[[[main+44B97D8]+48]+140]";
 
-        public static IReadOnlyList<long> NIDListPointer { get; } = new long[] { 0x44C7730, 0xF8, 0x8 };
+        public static IReadOnlyList<long> NIDListPointer { get; } = new long[] { 0x44DDC10, 0xF8, 0x8 };
     }
 }
